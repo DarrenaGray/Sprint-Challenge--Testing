280445e892ed8998f7ecd92c4627cffad1e1ec3d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-17 12:37:19 -0400

    Added additional testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,10 +6,21 @@ describe('server', () => {
 	describe('GET /', () => {
 		it('should return 200 status', async () => {
 			const expRes = 200;
-
 			const res = await request(server).get('/');
-
 			expect(res.status).toEqual(expRes);
+		});
+
+		it('should return JSON using done callback', done => {
+			request(server).get('/').then(res => {
+				expect(res.type).toBe('application/json');
+				done();
+			});
+		});
+		it('should return { api: "running" }', () => {
+			const expected = { api: 'running' };
+			return request(server).get('/').then(res => {
+				expect(res.body).toEqual(expected);
+			});
 		});
 	});
 });
