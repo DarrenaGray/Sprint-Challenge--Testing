4f2b881dcdae44524b3385df5e4584b617cbe070
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-17 13:10:43 -0400

    Fixed the test for adding a new game to the database

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -1,6 +1,11 @@
+const db = require('../data/dbConfig');
+
 const Games = require('./gamesModel');
 
 describe('games model', () => {
+	beforeEach(async () => {
+		await db('games').truncate();
+	});
 	describe('insert()', () => {
 		it('should add a game', async () => {
 			await Games.insert({
@@ -9,7 +14,7 @@ describe('games model', () => {
 				releasedYear: '2014'
 			});
 
-			const games = await request(Games);
+			const games = await db('games');
 
 			expect(games).toHaveLength(1);
 		});
